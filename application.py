7c5fe5c23cd41acc9418875eca30b2fe6b299edf
Yiwen Wang <yiwenwa@amazon.com>
2021-07-14 20:12:21 -0700

    Increase timeout and add handling of EndpointConnectionError

--- application.py
+++ application.py
@@ -1,15 +1,22 @@
 # from package.MovieTable import create_movie_table, put_item
 import boto3
 import flask
+import time
+from botocore.exceptions import EndpointConnectionError
 
 application = flask.Flask(__name__)
 
 @application.route('/')
 def home():
-  client = boto3.client('sts')
-  identity = client.get_caller_identity()
-
-  return identity['Arn']
+	start = time.time()
+	client = boto3.client('sts')
+	while (time.time() - start < 300):
+		try:
+			identity = client.get_caller_identity()
+			return identity['Arn']
+		except EndpointConnectionError:
+			time.sleep(5)
+			continue
 
 if __name__ == '__main__':
-  application.run(debug=True)
+	application.run(debug=True)
